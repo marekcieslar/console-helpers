6e8c92ff3c986e27a5e05f9938a08207097d1298
marek.cieslar <marek.cieslar@clurgo.com>
2025-01-27 18:42:19 +0100

    change way to merge

--- sh/git-merge-master-branch.sh
+++ sh/git-merge-master-branch.sh
@@ -3,14 +3,9 @@
 # alias
 # alias git:merge='bash ~/console-helpers/sh/git-merge-master-branch.sh'
 
-# Fetch the latest changes from the remote repository
-git fetch origin
-
-# Get the branch name from the first argument, default to 'master' if not provided
 BRANCH=${1:-master}
 
-# Update the local branch
-git merge origin/$BRANCH $BRANCH
-
-# Merge changes from the specified branch into the current branch
+git checkout $BRANCH
+git pull
+git checkout -
 git merge $BRANCH
